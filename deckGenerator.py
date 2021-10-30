ba9dcac469787880934621454437fb35e3086fd2
Arsha Ali <squirt999@gmail.com>
2021-10-30 17:21:26 -0400

    initialize decks

--- deckGenerator.py
+++ deckGenerator.py
@@ -1,4 +1,6 @@
 import random
+from collections import deque
+#from SolitaireClasses import Talon
 
 #Decision: Do we want the deck to be a list of tuples, 
 #like Deck = [("S", "2"), ...] or a list of strings like ["S2", ...], 
@@ -22,3 +24,52 @@ def deckGen2():
             Deck.append(i+j)
     random.shuffle(Deck)
     return Deck
+
+
+def reachableTalonGen(Deck):
+    reachableTalon = deque(Deck[27:46])
+    return reachableTalon
+
+def unreachableTalonGen(Deck):
+    unreachableTalon = deque(Deck[46:51])
+    return unreachableTalon
+
+def foundationGen():
+    foundation1 = [] #for spades
+    foundation2 = [] #for clubs
+    foundation3 = [] #for hearts
+    foundation4 = [] #for diamonds
+    Foundation = [foundation1,foundation2,foundation3,foundation4]
+    return Foundation
+
+def tableauGen(Deck):
+    tableau1 = [deque(Deck[0]), deque([])] #first array for face up, second array for face down 
+    tableau2 = [deque(Deck[1]), deque(Deck[2])]
+    tableau3 = [deque(Deck[3]), deque(Deck[4:6])]
+    tableau4 = [deque(Deck[6]), deque(Deck[7:10])]
+    tableau5 = [deque(Deck[10]), deque(Deck[11:15])]
+    tableau6 = [deque(Deck[15]), deque(Deck[15:20])]
+    tableau7 = [deque(Deck[20]), deque(Deck[21:27])]
+    Tableau = [tableau1,tableau2,tableau3,tableau4,tableau5,tableau6,tableau7]
+    return Tableau
+
+def printDeck(tableau,foundation,reachable_talon,unreachable_talon):
+    print("Tableau: ", tableau)
+    print("Foundation: ", foundation)
+    print("Reachable Talon: ", reachable_talon)
+    print("Unreachable Talon: ", unreachable_talon)
+
+def printDeckLength(tableau,foundation,reachable_talon,unreachable_talon):
+    tableauLength = []
+    for tableau_stack in tableau:
+        print(tableau_stack[0])
+        print(tableau_stack[1])
+        tableauLength.append(len(tableau_stack[0])+len(tableau_stack[1]))
+    foundationLength = []
+    for foundation_stack in foundation:
+        foundationLength.append(len(foundation_stack))
+    print("Tableau: ", tableauLength)
+    print("Foundation: ", foundationLength)
+    print("Reachable Talon: ", len(reachable_talon))
+    print("Unreachable Talon: ", len(unreachable_talon))
+    print("Total: ", sum(tableauLength)+sum(foundationLength)+len(reachable_talon)+len(unreachable_talon))
--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import numpy as np
 
 #Generate a random deck of cards
 deck = deckGenerator.deckGen1()
+#print(deck)
+#print(len(deck))
+
+tableau = deckGenerator.tableauGen(deck)
+foundation = deckGenerator.foundationGen()
+reachable_talon = deckGenerator.reachableTalonGen(deck)
+unreachable_talon = deckGenerator.unreachableTalonGen(deck)
+
+deckGenerator.printDeck(tableau,foundation,reachable_talon,unreachable_talon)
+deckGenerator.printDeckLength(tableau,foundation,reachable_talon,unreachable_talon)
+
 #Load up our classes
 
 #something like
@@ -12,19 +23,19 @@ def loadClasses(deck):
     #etc
     return 0
 
-loadClasses()
+#loadClasses()
 
 
 
 # A New State Representation: K+ Solitaire
 #This may not stay as a function, I'm just putting it here so there's somewhere to keep it
 #example talon queue for testing
-talon = queue.LifoQueue()
-talon.put("5D")
-talon.put("AS")
-talon.put("10H")
-talon.put("3C")
-talon.put("QH")
+#talon = queue.LifoQueue()
+#talon.put("5D")
+#talon.put("AS")
+#talon.put("10H")
+#talon.put("3C")
+#talon.put("QH")
 
 def compressedSearchTree():
     return 0
