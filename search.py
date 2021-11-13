fd02f321c8f8c47e4cf9d88a31511c91677f037c
JakeSchuchmann <92607729+JakeSchuchmann@users.noreply.github.com>
2021-11-13 15:06:38 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,20 +1,72 @@
 import numpy as np
 
+def opp_color_check(c1, c2):
+    diff_colors = False
+    
+    #Card 1 is red, Card 2 is black
+    if c1[0] == 'H' or c1[0] == 'D' and c2[0] == 'S' or c2[1] == 'C':
+        diff_colors = True
+    
+    #Card 1 is black, Card 2 is red
+    if c1[0] == 'S' or c1[0] == 'C' and c2[0] == 'H' or c2[1] == 'D':
+        diff_colors = True
+    
+    return diff_colors
 
 def get_actions(s):
-    actions = {}
-    '''
-    Should contain information on from and to:
+    actions = []
     
-    from: Which parent it belongs to (talon,tableu, etc) and its index in that parent
+    #See where Talon cards can be moved:
+    for tal_idx, card in enumerate(s.reachable_talon):
+        
+        #Talon to tableau
+        for tab_idx, stack in enumerate(s.tableau):
+            
+            #If different colors and tableau end is one number higher than talon card
+            if opp_color_check(card, stack[0]) and stack[0][1] - card[1] == 1:
+                new_entry = {'from':[0, tal_idx], 'to':[1,tab_idx,0]}
+                actions.append(new_entry)
+        
+        #Talon to foundation
+        for found_idx, stack in enumerate(s.foundation):
+            if stack[0][0] == card[0] and stack[0][1] - card[1] == 1: #If suits match and proper next card
+                new_entry = {'from':[0,tal_idx], 'to':[2,found_idx]}
+ #-------------------------------------------------------------------------------------------------------               
+    #See where tableau cards/stacks can be moved to
+    for stack_idx, stack in enumerate(s.tableau): #For each stack
+        
+        #Tableau to tableau:
+        for stack_depth in range(len(stack[0])): #For each card in a face up stack
+            card = stack[0][stack_depth]
+            
+            for si2, stack2 in enumerate(s.tableau): #Iterate through each card in stack
+                if opp_color_check(card,stack2[0][0]) and stack2[0][0][1] - card[1] == 1:
+                    #Append a card if stack_depth = 0, append a bundle of cards starting from stack_depth
+                    #if stack_depth > 0
+                    new_entry = {'from':[1,stack_idx,stack_depth], 'to':[1,si2,0]}
+                    actions.append(new_entry)
+                
+        
+            #If next card in stack can't be moved, stop looping through stack
+            if stack_depth < len(stack[0]):
+                if stack[0][stack_depth+1] - stack[0][stack_depth] != 1: 
+                    break
+                    
     
-    to: Which parent it going to, and its index in that parent.
+        #Tableau to foundation:
+        for found_idx, found_stack in enumerate(s.foundation):
+            if stack[0][0] == found_stack[0] and found_stack[1] - stack[0][1] == 1:
+                new_entry = {'from':[1,stack_idx,0], 'to':[2,found_idx]}
+ #------------------------------------------------------------------------------------------------------- 
+        #See where foundation cards can be moved to
+        for stack_idx, stack in enumerate(s.foundation):
+            #Foundation to tableau
+            for tab_idx, tab_stack in enumerate(s.tableau):
+                if opp_color_check(stack[0][0], tab_stack[0][0]) and tab_stack[0][1] - stack[0][1] == 1:
+                    #Move card from foundation to end of a tableau stack
+                    new_entry = {'from':[2,stack_idx], 'to':[1,tab_idx,0]} 
     
-    Ex:
-    a = {from:Parent,card, to:Parent,card}
-    Maybe a list of dictionaries?
-    or some structure similar to that
-    '''
+    
     return actions
 
 def result(s,a):
@@ -86,6 +138,7 @@ def mns_rollout(s, hs, ns, a):
                     best_a = act
             
             s = result(s,best_a)
+            #implement way to store the path we take
             
     return hs[0](s)
             
